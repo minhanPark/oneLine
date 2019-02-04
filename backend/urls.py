8a5011681ff8f4e90aec133d303c49d9b0da20a8
minhan <alsgks1218@gmail.com>
2019-02-04 14:50:35 +0900

    setting django template and rendered index.html

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.views.generic import TemplateView
 from oneLine import views
 
 router = routers.DefaultRouter()
@@ -23,5 +24,6 @@ router.register('wisesaying', views.WiseSayingView, 'wisesaying')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
